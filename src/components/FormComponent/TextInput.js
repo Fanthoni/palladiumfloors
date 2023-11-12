ef16cb5b5b7e5d87315cf5b215d05b49bf6a5ce8
ferrelanthoni <ferrelanthoni@gmail.com>
2023-11-11 20:08:05 -0800

    add padding to bottom of label

--- src/components/FormComponent/TextInput.js
+++ src/components/FormComponent/TextInput.js
@@ -1,36 +1,61 @@
 import "./TextInput.css";
 
-function TextBox({displayName, fieldName, isRequired = false, ...props}) {
-    return (
-        <div {...props}>
-            <label htmlFor={fieldName}>
-                {displayName} {isRequired ? <span>*</span> : null}
-            </label><br />
-            <input className="roundCorner" type="text" name={fieldName} id={fieldName} required={isRequired ?? false}/>
-        </div>
-    );
+function TextBox({ displayName, fieldName, isRequired = false, ...props }) {
+  return (
+    <div {...props}>
+      <label htmlFor={fieldName}>
+        {displayName} {isRequired ? <span>*</span> : null}
+      </label>
+      <input
+        className="roundCorner"
+        type="text"
+        name={fieldName}
+        id={fieldName}
+        required={isRequired ?? false}
+      />
+    </div>
+  );
 }
 
-function NumericalTextBox({displayName, fieldName, isRequired = false}) {
-    return (
-        <div>
-            <label htmlFor={fieldName}>
-                {displayName} { isRequired ? <span>*</span> : null}
-            </label><br />
-            <input className="roundCorner" type="number" name={fieldName} id={fieldName} required={isRequired ?? false}/>
-        </div>
-    );
+function NumericalTextBox({ displayName, fieldName, isRequired = false }) {
+  return (
+    <div>
+      <label htmlFor={fieldName}>
+        {displayName} {isRequired ? <span>*</span> : null}
+      </label>
+      <input
+        className="roundCorner"
+        type="number"
+        name={fieldName}
+        id={fieldName}
+        required={isRequired ?? false}
+      />
+    </div>
+  );
 }
 
-function TextArea({displayName, fieldName, isRequired = false, maxLength="1024", ...props}) {
-    return (
-        <div {...props}>
-            <label htmlFor={fieldName}>
-                {displayName} {isRequired ? <span>*</span> : null}
-            </label><br />
-            <textarea className="roundCorner" type="text" name={fieldName} id={fieldName} maxLength={maxLength} required={isRequired ?? false}/>
-        </div>
-    );
+function TextArea({
+  displayName,
+  fieldName,
+  isRequired = false,
+  maxLength = "1024",
+  ...props
+}) {
+  return (
+    <div {...props}>
+      <label htmlFor={fieldName}>
+        {displayName} {isRequired ? <span>*</span> : null}
+      </label>
+      <textarea
+        className="roundCorner"
+        type="text"
+        name={fieldName}
+        id={fieldName}
+        maxLength={maxLength}
+        required={isRequired ?? false}
+      />
+    </div>
+  );
 }
 
 export { TextBox, NumericalTextBox, TextArea };
