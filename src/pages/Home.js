f5e0295c029d7e60214f508fac25652f60b236b6
ferrelanthoni <ferrelanthoni@gmail.com>
2023-11-15 23:25:05 -0800

    added some animation on the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,181 +13,238 @@ function importAll(r) {
   }
 
 function Home() {
-    const cardImages = importAll(require.context('../assets/cardImages', false, /\.(png|jpe?g)$/));
-    let galleryImages = importAll(require.context('../assets/galleryImages', false, /\.(png|jpe?g)$/));
-    galleryImages = _.values(galleryImages);
+  const cardImages = importAll(
+    require.context("../assets/cardImages", false, /\.(png|jpe?g)$/)
+  );
+  let galleryImages = importAll(
+    require.context("../assets/galleryImages", false, /\.(png|jpe?g)$/)
+  );
+  galleryImages = _.values(galleryImages);
 
-    return (
-        <Container>
-            <PageContainer>
-                <NavigationContainer >
-                    <span>
-                        <img id="logo" src={logo} alt="logo" style={{display:"block"}}/>
-                    </span>
-                    <Menu />
-                </NavigationContainer>
+  return (
+    <div
+      style={{
+        justifyContent: "center",
+        display: "flex",
+      }}
+    >
+      <Container>
+        <PageContainer>
+          <NavigationContainer>
+            <span>
+              <img
+                id="logo"
+                src={logo}
+                alt="logo"
+                style={{ display: "block" }}
+              />
+            </span>
+            <Menu />
+          </NavigationContainer>
 
-                <IntroductoryContainer>
-                    <h1 id="title">
-                        Adding Elegance and Expertise to Your Space            
-                    </h1>
-                    <p id="description">
-                        we bring touch of elegance and a wealth of expertise to  your floors, ensuring that every step you take is a step in luxury and style
-                    </p>
+          <IntroductoryContainer>
+            <h1 id="title">Adding Elegance and Expertise to Your Space</h1>
+            <p id="description">
+              we bring touch of elegance and a wealth of expertise to your
+              floors, ensuring that every step you take is a step in luxury and
+              style
+            </p>
 
-                    <ContactContainer>
-                        <Button 
-                            variant="contained"
-                            sx={{borderRadius: 5}}
-                            className="contact-btn"
-                        >
-                            Call Now
-                        </Button>
-                        <Button 
-                            variant="contained"
-                            sx={{borderRadius: 5}}
-                            className="contact-btn"
-                        >
-                            <EmailIcon />
-                        </Button>
-                    </ContactContainer>
+            <ContactContainer>
+              <Button
+                variant="contained"
+                sx={{
+                  borderRadius: 5,
+                  "&:hover": {
+                    backgroundColor: "black",
+                  },
+                }}
+                className="contact-btn"
+              >
+                Call Now
+              </Button>
+              <Button
+                variant="contained"
+                sx={{
+                  borderRadius: 5,
+                  "&:hover": {
+                    backgroundColor: "black",
+                  },
+                }}
+                className="contact-btn"
+              >
+                <EmailIcon />
+              </Button>
+            </ContactContainer>
+          </IntroductoryContainer>
 
-                </IntroductoryContainer>
-
-                <SliderContainer>
-                    <FlooringCard backgroundImage={cardImages["hardwood.jpeg"]} title="Hardwood"/>
-                    <FlooringCard backgroundImage={cardImages["vinyl.jpeg"]} title="Vinyl"/>
-                    <FlooringCard backgroundImage={cardImages["laminated.jpeg"]} title="Laminated"/>
-                    <FlooringCard backgroundImage={cardImages["others.jpeg"]} title="View all"/>
-                </SliderContainer>
-            </PageContainer>
-            <GalleryContainer>
-                <Gallery items={galleryImages}/>
-            </GalleryContainer>
-        </Container>
-
-    );
+          <SliderContainer>
+            <FlooringCard
+              backgroundImage={cardImages["hardwood.jpeg"]}
+              title="Hardwood"
+            />
+            <FlooringCard
+              backgroundImage={cardImages["vinyl.jpeg"]}
+              title="Vinyl"
+            />
+            <FlooringCard
+              backgroundImage={cardImages["laminated.jpeg"]}
+              title="Laminated"
+            />
+            <FlooringCard
+              backgroundImage={cardImages["others.jpeg"]}
+              title="View all"
+            />
+          </SliderContainer>
+        </PageContainer>
+        <GalleryContainer></GalleryContainer>
+        <Gallery items={galleryImages} />
+      </Container>
+    </div>
+  );
 }
 
 const Container = styled.div`
-    margin: 2rem;
+  margin: 2rem;
 
-    display: flex;
-    flex-direction: row;
-    gap: 20px;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  gap: 20px;
+  max-width: 1000px;
 
-    @media (max-width: 768px) {
-        flex-direction: column-reverse;
+  @media (max-width: 768px) {
+    flex-direction: column-reverse;
 
-        & > * {
-            width: 100% !important;
-        }
+    & > * {
+      width: 100% !important;
     }
+  }
 `;
 
 const PageContainer = styled.div`
-    width: 50%;
+  width: 50%;
 
-    Button {
-        text-transform: none;
-    }
+  Button {
+    text-transform: none;
+  }
 `;
 
 const NavigationContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 
-    #logo {
-        width: 150px;
-    }
+  #logo {
+    width: 150px;
+  }
 `;
 
 const IntroductoryContainer = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-    #title, #description {
-        width: 80%;
-    }
+  #title,
+  #description {
+    width: 80%;
+  }
 
-    #description {
-        margin-top: 0;
-        margin-bottom: 2rem;
-    }
+  #description {
+    margin-top: 0;
+    margin-bottom: 2rem;
+  }
 
-    .contact-btn {
-        background-color: black;
-    }
+  .contact-btn {
+    background-color: black;
+  }
 `;
 
 const ContactContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    gap: 10px;
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
 `;
 
 const SliderContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    gap: 20px;
-    overflow: auto;
-    width: 90%;
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  overflow: auto;
+  width: 90%;
 
-    margin-top: 2rem;
+  margin-top: 2rem;
+  padding-top: 1rem;
 
-    &::-webkit-scrollbar {
-        display: none;
-    }
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 const GalleryContainer = styled.div`
-    width: 50%;
+  width: 50%;
 
-    @media (max-width: 768px) {
-        transform: translate(-10px);
-    }
+  @media (max-width: 768px) {
+    transform: translate(-10px);
+  }
 `;
 
-function FlooringCard({backgroundImage, title, href}) {
-    return (
-        <CardContainer
-            style={{
-                backgroundImage: `url(${backgroundImage})`,
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat"
-            }}
-        >
-            <Button 
-                variant="contained"
-                sx={{borderRadius: 5}}
-                size="small"
-            >
-                {title}
-            </Button>
-        </CardContainer>
-    );
+function FlooringCard({ backgroundImage, title, href }) {
+  return (
+    <CardContainer
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Button
+        variant="contained"
+        sx={{
+          borderRadius: 5,
+          "&:hover": {
+            backgroundColor: "white",
+          },
+        }}
+        size="small"
+      >
+        {title}
+      </Button>
+    </CardContainer>
+  );
 }
 
 const CardContainer = styled.div`
-    height: 150px;
-    min-width: 80px;
-    padding: 1rem;
+  height: 150px;
+  min-width: 80px;
+  padding: 1rem;
+  cursor: pointer;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    align-items: center;
-
-    border-radius: 10px;
+  &:hover {
+    transform: translateY(-5px);
+    transition: transform 0.5s ease;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
 
-    Button {
-        background-color: white;
-        color: black;
-    }
-`
+  &:not(:hover) {
+    transform: translateY(0);
+    transition: transform 0.5s ease;
+    box-shadow: none;
+  }
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+
+  border-radius: 10px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+  Button {
+    background-color: white;
+    color: black;
+  }
+`;
 
 function Gallery({items}) {
     const imageLimitCount = window.innerWidth > 768 ? 4 : 2;
